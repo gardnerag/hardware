3433f674dfa278bc0252d704cba85ec0508d7b2d
emily <emily.gardner@gardner-ag.com>
2020-04-08 09:50:47 +0200

    updated

--- test.py
+++
@@ -1,9 +0,0 @@
-class test():
-    def __init__(self):
-        print('In constructor')
-    
-    def __del__(self):
-        print('In destructor')
-
-if __name__ == "__main__":
-    testt = test()
